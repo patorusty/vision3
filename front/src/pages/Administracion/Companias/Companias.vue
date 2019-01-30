b782866a39ffa9717732bd602d83e24f54368c40
Air <air@MacBook-Air-de-Air.local>
2019-01-30 19:02:48 -0300

    tablas carpetas y modal

---
+++ front/src/pages/Administracion/Companias/Companias.vue
@@ -0,0 +1,218 @@
+<template>
+  <div class="content">
+    <div class="row mt-5">
+      <div class="col-12">
+        <card card-body-classes="table-full-width">
+          <router-link
+            slot="header"
+            to=""
+          >
+            <base-button
+              class="animation-on-hover pull-right"
+              type="primary"
+            >Crear</base-button>
+          </router-link>
+          <div>
+            <div class="col-12 d-flex justify-content-center justify-content-sm-between flex-wrap">
+              <el-select
+                class="select-primary mb-3 pagination-select"
+                v-model="pagination.perPage"
+                placeholder="Per page"
+              >
+                <el-option
+                  class="select-primary"
+                  v-for="item in pagination.perPageOptions"
+                  :key="item"
+                  :label="item"
+                  :value="item"
+                ></el-option>
+              </el-select>
+              <base-input>
+                <el-input
+                  type="search"
+                  class="mb-3 search-input"
+                  clearable
+                  prefix-icon="el-icon-search"
+                  placeholder="Buscar"
+                  v-model="searchQuery"
+                  aria-controls="datatables"
+                ></el-input>
+              </base-input>
+            </div>
+            <el-table :data="queriedData">
+              <el-table-column
+                label="Nombre"
+                prop="nombre"
+                sortable
+                :min-width="80"
+              ></el-table-column>
+              <el-table-column
+                label="Telefono"
+                prop="telefono_1"
+                :min-width="80"
+              ></el-table-column>
+              <el-table-column
+                label="Tel. Auxilio"
+                prop="telefono_aux"
+                :min-width="80"
+              ></el-table-column>
+              <el-table-column
+                label="Tel. Siniestro"
+                prop="telefono_siniestros"
+                :min-width="100"
+              ></el-table-column>
+              <el-table-column
+                label="Activo"
+                prop="activo"
+              >
+                <div slot-scope="{ row }">
+                  <div v-if="row.activo == 1">SI</div>
+                  <div v-else>NO</div>
+                </div>
+              </el-table-column>
+              <el-table-column
+                align="right"
+                label="Actions"
+              >
+                <div slot-scope="props">
+                  <base-button
+                    @click.native="handleLike(props.$index, props.row);"
+                    class="remove btn-link"
+                    type="info"
+                    size="sm"
+                    icon
+                  >
+                    <i class="tim-icons icon-heart-2"></i>
+                  </base-button>
+                  <base-button
+                    @click.native="handleEdit(props.$index, props.row);"
+                    class="edit btn-link"
+                    type="warning"
+                    size="sm"
+                    icon
+                  >
+                    <i class="tim-icons icon-pencil"></i>
+                  </base-button>
+                  <base-button
+                    @click.native="handleDelete(props.$index, props.row);"
+                    class="remove btn-link"
+                    type="danger"
+                    size="sm"
+                    icon
+                  >
+                    <i class="tim-icons icon-simple-remove"></i>
+                  </base-button>
+                </div>
+              </el-table-column>
+            </el-table>
+          </div>
+          <div
+            slot="footer"
+            class="col-12 d-flex justify-content-center justify-content-sm-between flex-wrap"
+          >
+            <div class>
+              <p class="card-category">Showing {{ from + 1 }} to {{ to }} of {{ total }} entries</p>
+            </div>
+            <base-pagination
+              class="pagination-no-border"
+              v-model="pagination.currentPage"
+              :per-page="pagination.perPage"
+              :total="total"
+            ></base-pagination>
+          </div>
+        </card>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import { Table, TableColumn, Select, Option } from 'element-ui';
+import { BasePagination } from 'src/components';
+import Fuse from 'fuse.js';
+import axios from 'axios';
+
+export default {
+  components: {
+    BasePagination,
+    [Select.name]: Select,
+    [Option.name]: Option,
+    [Table.name]: Table,
+    [TableColumn.name]: TableColumn
+  },
+  computed: {
+    /***
+     * Returns a page from the searched data or the whole data. Search is performed in the watch section below
+     */
+    queriedData() {
+      let result = this.tableData;
+      if (this.searchedData.length > 0) {
+        result = this.searchedData;
+      }
+      return result.slice(this.from, this.to);
+    },
+    to() {
+      let highBound = this.from + this.pagination.perPage;
+      if (this.total < highBound) {
+        highBound = this.total;
+      }
+      return highBound;
+    },
+    from() {
+      return this.pagination.perPage * (this.pagination.currentPage - 1);
+    },
+    total() {
+      return this.searchedData.length > 0
+        ? this.searchedData.length
+        : this.tableData.length;
+    }
+  },
+  data() {
+    return {
+      pagination: {
+        perPage: 5,
+        currentPage: 1,
+        perPageOptions: [5, 10, 25, 50],
+        total: 0
+      },
+      searchQuery: '',
+      propsToSearch: [],
+      tableData: [],
+      searchedData: [],
+      fuseSearch: null
+    };
+  },
+  methods: {
+    cargaPolizas() {
+      axios
+        .get('http://127.0.0.1:8000/api/administracion/companias/')
+        .then(response => {
+          console.log(response.data.data);
+          this.dataLoaded = true;
+          this.tableData = response.data.data;
+        });
+    }
+  },
+  mounted() {
+    this.fuseSearch = new Fuse(this.tableData, {
+      keys: [],
+      threshold: 0.3
+    });
+
+    this.cargaPolizas();
+  },
+  watch: {
+    /**
+     * Searches through the table data by a given query.
+     * NOTE: If you have a lot of data, it's recommended to do the search on the Server Side and only display the results here.
+     * @param value of the query
+     */
+    searchQuery(value) {
+      let result = this.tableData;
+      if (value !== '') {
+        result = this.fuseSearch.search(this.searchQuery);
+      }
+      this.searchedData = result;
+    }
+  }
+};
+</script>
--- front/src/routes/routes.js
+++ front/src/routes/routes.js
@@ -5,8 +5,8 @@ import NotFound from 'src/pages/GeneralViews/NotFoundPage.vue';
 const Home = () =>
   import('src/pages/Home.vue');
 
-const Automotor = () =>
-  import('src/pages/Automotor.vue');
+// const Automotor = () =>
+//   import('src/pages/Automotor.vue');
 
 
 let otrosRiesgos = {
@@ -15,20 +15,20 @@ let otrosRiesgos = {
   name: 'Otros Riesgos',
   redirect: '/otrosriesgos/accidentepersonal',
   children: [{
-      path: 'accidentepersonal',
-      name: 'Accidente Personal',
-      component: require('../pages/OtrosRiesgos/AccidentePersonal.vue').default
-    },
-    {
-      path: 'combinadofamiliar',
-      name: 'Combinado Familiar',
-      component: require('../pages/OtrosRiesgos/CombinadoFamiliar.vue').default
-    },
-    {
-      path: 'otros',
-      name: 'Otros',
-      component: require('../pages/OtrosRiesgos/Otros.vue').default
-    }
+    path: 'accidentepersonal',
+    name: 'Accidente Personal',
+    component: require('../pages/Polizas/OtrosRiesgos/AccidentePersonal.vue').default
+  },
+  {
+    path: 'combinadofamiliar',
+    name: 'Combinado Familiar',
+    component: require('../pages/Polizas/OtrosRiesgos/CombinadoFamiliar.vue').default
+  },
+  {
+    path: 'otros',
+    name: 'Otros',
+    component: require('../pages/Polizas/OtrosRiesgos/Otros.vue').default
+  }
   ]
 }
 
@@ -38,35 +38,35 @@ let administracion = {
   name: 'Administracion',
   redirect: '/administracion/companias',
   children: [{
-      path: 'companias',
-      name: 'Companias',
-      component: require('../pages/Administracion/Companias.vue').default
-    },
-    {
-      path: 'organizadores',
-      name: 'Organizadores',
-      component: require('../pages/Administracion/Organizadores.vue').default
-    },
-    {
-      path: 'productores',
-      name: 'Productores',
-      component: require('../pages/Administracion/Productores.vue').default
-    },
-    {
-      path: 'librosrubricados',
-      name: 'libros Rubricados',
-      component: require('../pages/Administracion/LibrosRubricados.vue').default
-    },
-    {
-      path: 'estadisticas',
-      name: 'Estadisticas',
-      component: require('../pages/Administracion/Estadisticas.vue').default
-    },
-    {
-      path: 'marcamodelos',
-      name: 'Marca Modelos',
-      component: require('../pages/Administracion/MarcaModelos.vue').default
-    }
+    path: 'companias',
+    name: 'Companias',
+    component: require('../pages/Administracion/Companias/Companias.vue').default
+  },
+  {
+    path: 'organizadores',
+    name: 'Organizadores',
+    component: require('../pages/Administracion/Organizadores/Organizadores.vue').default
+  },
+  {
+    path: 'productores',
+    name: 'Productores',
+    component: require('../pages/Administracion/Productores/Productores.vue').default
+  },
+  {
+    path: 'librosrubricados',
+    name: 'libros Rubricados',
+    component: require('../pages/Administracion/LibrosRubricados.vue').default
+  },
+  {
+    path: 'estadisticas',
+    name: 'Estadisticas',
+    component: require('../pages/Administracion/Estadisticas.vue').default
+  },
+  {
+    path: 'marcamodelos',
+    name: 'Marca Modelos',
+    component: require('../pages/Administracion/MarcaModelos.vue').default
+  }
   ]
 };
 
@@ -85,45 +85,45 @@ let configuracion = {
 
 
 const routes = [{
-    path: '/',
-    redirect: '/home',
-    name: 'Inicio',
-  },
+  path: '/',
+  redirect: '/home',
+  name: 'Inicio',
+},
   administracion,
   otrosRiesgos,
   configuracion,
+{
+  path: '/',
+  component: DashboardLayout,
+  redirect: '/home',
+  name: 'Layout',
+  children: [{
+    path: 'home',
+    name: 'Home',
+    component: require('../pages/Home.vue').default
+  },
   {
-    path: '/',
-    component: DashboardLayout,
-    redirect: '/home',
-    name: 'Layout',
-    children: [{
-        path: 'home',
-        name: 'Home',
-        component: require('../pages/Home.vue').default
-      },
-      {
-        path: '/polizas/automotor',
-        component: require('../pages/Automotor.vue').default,
-        name: 'Automotor'
-      },
-      {
-        path: 'siniestros',
-        name: 'Siniestros',
-        component: require('../pages/Siniestros.vue').default
-      },
-      {
-        path: 'endosos',
-        name: 'Endosos',
-        component: require('../pages/Endosos.vue').default
-      },
-      {
-        path: 'clientes',
-        name: 'Clientes',
-        component: require('../pages/Clientes.vue').default
-      }
-    ]
+    path: '/polizas/automotor',
+    component: require('../pages/Polizas/Automotor/Automotor.vue').default,
+    name: 'Automotor'
+  },
+  {
+    path: 'siniestros',
+    name: 'Siniestros',
+    component: require('../pages/Polizas/Siniestros.vue').default
+  },
+  {
+    path: 'endosos',
+    name: 'Endosos',
+    component: require('../pages/Polizas/Endosos.vue').default
+  },
+  {
+    path: 'clientes',
+    name: 'Clientes',
+    component: require('../pages/Clientes/Clientes.vue').default
   }
+  ]
+}
 ];
 
 export default routes;
