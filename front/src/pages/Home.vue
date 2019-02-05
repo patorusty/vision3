ce214c87f0fb7053790052244685c74d38dd1b4f
rusty <patriciosarno@MacBook-Pro-de-Patricio.local>
2019-02-05 14:29:01 -0300

    3er

--- front/src/pages/Home.vue
+++ front/src/pages/Home.vue
@@ -1,10 +1,27 @@
 <template>
-  <h1>Home</h1>
+<div>
+  <h1>Home </h1>
+  <p class="iconos">
+  <icon-base  icon-name="car" width="32" height="32" vB="0 0 32 32" iconColor="blue"><icon-car /></icon-base>
+  <icon-base  icon-name="moon" width="32" height="32"><icon-moon /></icon-base>
+
+  </p>
+
+
+</div>
 </template>
 
 <script>
 import axios from 'axios';
+import IconBase from '../components/Icons/IconBase';
+import IconCar from '../components/Icons/IconCar';
+import IconMoon from '../components/Icons/IconMoon';
 export default {
+  components: {
+    IconBase,
+    IconCar,
+    IconMoon
+  },
   methods: {
     prueba() {
       axios.get('http://127.0.0.1:8000/api/clientes').then(res => {
@@ -19,8 +36,9 @@ export default {
 </script>
 <style>
 h1 {
-  background-color: red;
+  color: black;
+}
+.iconos {
+  color: red;
 }
 </style>
-
-
