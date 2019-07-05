bc1b353109ff68228a9ed291b2ab6d04f2055d0d
Boti <Boti@MacBook-Air-de-Air.local>
2019-07-05 16:45:08 -0300

    renovaciones

--- front/src/pages/Polizas/Riesgos/Automotor/ ModalRenovarAutomotor.vue
+++ front/src/pages/Polizas/Riesgos/Automotor/ ModalRenovarAutomotor.vue
@@ -1,9 +1,13 @@
 <template>
   <SlideYUpTransition :duration="500">
-    <div class="modal-backdrop">
+    <div
+      class="modal-backdrop"
+      @keydown.esc="close"
+      @click="close"
+    >
       <div
         @click.stop
-        style="width:40%;"
+        style="width:60%;"
       >
         <card>
           <form>
@@ -13,12 +17,39 @@
                 class="close"
                 type="button"
                 aria-label="Close"
+                @click="close"
               >
                 <span aria-hidden="true">&times;</span>
               </button>
             </div>
             <div class="modal-contenido">
+              <div class="col-md-12">
+                <div class="row">
+                  <div class="col-md-6">
+                    <div class="row">
 
+                      <p class="text-primary">Asegurado: </p>
+                      <p class="">Juan Carlos Perez</p>
+                    </div>
+                    &nbsp;
+                    <div class="row">
+                      <p class="text-primary">Compañia: Sancor</p>
+                      <p class="">Sancor</p>
+                    </div>
+                  </div>
+                  <div class="col-md-6">
+                    <div class="row">
+                      <p class="text-primary">Renueva Poliza: </p>
+                      <p class="">90909282</p>
+                    </div>
+                    &nbsp;
+                    <div class="row">
+                      <p class="text-primary">Productor</p>
+                      <p class="">Juan Ignacio Sarno (Cod.1234)</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </form>
         </card>
@@ -44,6 +75,12 @@ export default {
     [Option.name]: Option,
     [Select.name]: Select,
     [DatePicker.name]: DatePicker
+  },
+  methods: {
+    close() {
+      this.$emit('close');
+      EventBus.$emit('resetInput', false);
+    }
   }
 };
 </script>
